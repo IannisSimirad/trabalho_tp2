271e4187c5049bc3139bebcb30b1c648522525bf
AndrebBritooo <a85226@ualg.pt>
2023-11-30 17:08:57 +0000

    Apagar comentários veiculos.py

--- base/src/veiculos.py
+++ base/src/veiculos.py
@@ -2,11 +2,19 @@ from io_terminal import imprime_lista
 
 nome_ficheiro_lista_de_veiculos = "lista_de_veiculos.pk"
 
+def cria_novo_veiculo():
 
-# TODO: Copie para aqui o código de cada uma das funções nos
-# ficheiros com o nome veiculos-*.py e faça um commit de cada vez
-# Quando este ficheiro estiver completo com todas as suas funções,
-# deve ser o unico ficheiro veiculos.py existente, deve apagar
-# todos os outros ficheiros veiculos-*.py, e inclusive estes comentários
 
-# ...
+    marca = input("marca? ")
+    matricula = input("matricula? ").upper()
+    
+
+    veiculo = {"marca": marca,
+               "matricula": matricula}
+
+    return veiculo
+
+def imprime_lista_de_veiculos(lista_de_veiculos):
+   
+
+    imprime_lista(cabecalho="Lista de Veiculos", lista=lista_de_veiculos)
