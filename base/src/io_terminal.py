77be40afdd2ccf80744183ad8f3fd1a0d565f07f
IannisSimirad <a85268@ualg.pt>
2023-11-30 16:50:47 +0000

    Adicionar função pergunta_id.py

--- base/src/io_terminal.py
+++ base/src/io_terminal.py
@@ -35,6 +35,25 @@ def imprime_lista(cabecalho, lista):
 
     input("Pressione ENTER para continuar...")
 
+    def pergunta_id(questao, lista, mostra_lista=False):
+    """TODO: documentação
+
+    :param questao:
+    :param lista:
+    :param mostra_lista:
+    :return:
+    """
+
+    if mostra_lista:
+        imprime_lista(cabecalho="", lista=lista)
+
+    while True:
+        id = int(input(questao))
+        if 0 <= id < len(lista):
+            return id
+        else:
+            print(f"id inexistente. Tente de novo. Valores admitidos {0} - {len(lista)}")
+
 # TODO: Copie para aqui o código de cada uma das funções nos
 # ficheiros com o nome io_terminal*.py e faça um commit de cada vez
 # Quando este ficheiro estiver completo com todas as suas funções,
