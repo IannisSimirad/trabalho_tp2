fb9499d6b465b2824e26271ede29841d1b0861f9
AndrebBritooo <a85226@ualg.pt>
2024-01-07 21:28:01 +0000

    Documentação do codigo do ficheiro:faturas.py

--- base/src/faturas.py
+++ base/src/faturas.py
@@ -1,31 +1,42 @@
 from datetime import date
-
 from io_terminal import imprime_lista, pergunta_id
 
 nome_ficheiro_lista_de_faturas = "lista_de_faturas.pk"
 
 def cria_nova_fatura(lista_de_clientes, lista_de_veiculos):
-    """Pede ao utilizador para introduzir os dados de uma nova fatura
-
-    :return: dicionario com uma fatura, na forma
-        {"cliente": <<id_cliente>>, "veiculo": <<id_veiculo>>, "data": <<data>>, ...}
     """
+    Pede ao utilizador para introduzir os dados de uma nova fatura.
 
+    :param lista_de_clientes: Lista de clientes disponíveis.
+    :type lista_de_clientes: list
+    :param lista_de_veiculos: Lista de veículos disponíveis.
+    :type lista_de_veiculos: list
+    :return: Um dicionário com os dados da nova fatura,
+             {"cliente": <<id_cliente>>, "veiculo": <<id_veiculo>>, "data": <<data>>, ...}
+    :rtype: dict
+    """
     id_cliente = pergunta_id(questao="Qual o id do cliente?", lista=lista_de_clientes, mostra_lista=True)
     id_veiculo = pergunta_id(questao="Qual o id do veiculo?", lista=lista_de_veiculos, mostra_lista=True)
 
-
-    fatura = {"cliente": id_cliente,
-              "veiculo": id_veiculo,
-              "data": date.today()}
+    fatura = {
+        "cliente": id_cliente,
+        "veiculo": id_veiculo,
+        "data": date.today()
+    }
 
     return fatura
 
-
 def imprime_lista_de_faturas(lista_de_faturas):
-    """TODO: documentação"""
+    """
+    Imprime a lista de faturas.
 
+    :param lista_de_faturas: Lista de faturas a ser impressa.
+    :type lista_de_faturas: list
+    """
+    # TODO: Adicionar detalhes sobre o formato da lista_de_faturas
+    # e como a função imprime_lista pode ser usada aqui.
     pass
+
 
 
 
