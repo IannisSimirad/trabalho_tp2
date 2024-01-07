725957f3249ac603fae71dee02cb6e6cbed6e6ac
AndrebBritooo <a85226@ualg.pt>
2024-01-07 21:26:54 +0000

    Documentação do codigo do ficheiro:clientes.py

--- base/src/clientes.py
+++ base/src/clientes.py
@@ -3,15 +3,22 @@ from io_terminal import imprime_lista
 nome_ficheiro_lista_de_clientes = "lista_de_clientes.py"
 
 def cria_novo_cliente():
-    """Pedir os dados de um novo cliente
-
-    :return: dicionario com o novo cliente, {"nome": <<nome>>, "nif": <<nif>>, ...}
     """
+    Pedir os dados de um novo cliente.
 
+    :return: Um dicionário com o novo cliente,
+             {"nome": <<nome>>, "nif": <<nif>>, ...}
+    :rtype: dict
+    """
     pass
 
 def imprime_lista_de_clientes(lista_de_clientes):
-    """TODO: documentação"""
+    """
+    Imprime a lista de clientes.
 
-
-
+    :param lista_de_clientes: Uma lista de clientes a ser impressa.
+    :type lista_de_clientes: list
+    """
+    # TODO: Adicionar detalhes sobre o formato da lista_de_clientes
+    # e como a função imprime_lista pode ser usada aqui.
+    pass
